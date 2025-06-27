510671f781799482cf32270bbe9d01ff70f97ace
Charwekey <crabisabutey@gmail.com>
2025-06-27 11:16:41 +0000

    All flies

---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout (() => {
+        document.querySelector(".container").style.opacity = "1";
+    }, 100);
+    
+}) ;
+
+
+
+
+
+
+
+
+
+
+
